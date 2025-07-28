fb78f0af38a7bfde92b2e3dec6cf9f5b0ebb060b
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-07-29 09:52:32 +1000

    added result

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -51,5 +51,156 @@ LIMIT 25
 
 
 /*
-
+[
+  {
+    "skill_id": 8,
+    "skills": "go",
+    "demanded_count": "27",
+    "avg_salary": "115319.89"
+  },
+  {
+    "skill_id": 234,
+    "skills": "confluence",
+    "demanded_count": "11",
+    "avg_salary": "114209.91"
+  },
+  {
+    "skill_id": 97,
+    "skills": "hadoop",
+    "demanded_count": "22",
+    "avg_salary": "113192.57"
+  },
+  {
+    "skill_id": 80,
+    "skills": "snowflake",
+    "demanded_count": "37",
+    "avg_salary": "112947.97"
+  },
+  {
+    "skill_id": 74,
+    "skills": "azure",
+    "demanded_count": "34",
+    "avg_salary": "111225.10"
+  },
+  {
+    "skill_id": 77,
+    "skills": "bigquery",
+    "demanded_count": "13",
+    "avg_salary": "109653.85"
+  },
+  {
+    "skill_id": 76,
+    "skills": "aws",
+    "demanded_count": "32",
+    "avg_salary": "108317.30"
+  },
+  {
+    "skill_id": 4,
+    "skills": "java",
+    "demanded_count": "17",
+    "avg_salary": "106906.44"
+  },
+  {
+    "skill_id": 194,
+    "skills": "ssis",
+    "demanded_count": "12",
+    "avg_salary": "106683.33"
+  },
+  {
+    "skill_id": 233,
+    "skills": "jira",
+    "demanded_count": "20",
+    "avg_salary": "104917.90"
+  },
+  {
+    "skill_id": 79,
+    "skills": "oracle",
+    "demanded_count": "37",
+    "avg_salary": "104533.70"
+  },
+  {
+    "skill_id": 185,
+    "skills": "looker",
+    "demanded_count": "49",
+    "avg_salary": "103795.30"
+  },
+  {
+    "skill_id": 2,
+    "skills": "nosql",
+    "demanded_count": "13",
+    "avg_salary": "101413.73"
+  },
+  {
+    "skill_id": 1,
+    "skills": "python",
+    "demanded_count": "236",
+    "avg_salary": "101397.22"
+  },
+  {
+    "skill_id": 5,
+    "skills": "r",
+    "demanded_count": "148",
+    "avg_salary": "100498.77"
+  },
+  {
+    "skill_id": 78,
+    "skills": "redshift",
+    "demanded_count": "16",
+    "avg_salary": "99936.44"
+  },
+  {
+    "skill_id": 187,
+    "skills": "qlik",
+    "demanded_count": "13",
+    "avg_salary": "99630.81"
+  },
+  {
+    "skill_id": 182,
+    "skills": "tableau",
+    "demanded_count": "230",
+    "avg_salary": "99287.65"
+  },
+  {
+    "skill_id": 197,
+    "skills": "ssrs",
+    "demanded_count": "14",
+    "avg_salary": "99171.43"
+  },
+  {
+    "skill_id": 92,
+    "skills": "spark",
+    "demanded_count": "13",
+    "avg_salary": "99076.92"
+  },
+  {
+    "skill_id": 13,
+    "skills": "c++",
+    "demanded_count": "11",
+    "avg_salary": "98958.23"
+  },
+  {
+    "skill_id": 186,
+    "skills": "sas",
+    "demanded_count": "63",
+    "avg_salary": "98902.37"
+  },
+  {
+    "skill_id": 7,
+    "skills": "sas",
+    "demanded_count": "63",
+    "avg_salary": "98902.37"
+  },
+  {
+    "skill_id": 61,
+    "skills": "sql server",
+    "demanded_count": "35",
+    "avg_salary": "97785.73"
+  },
+  {
+    "skill_id": 9,
+    "skills": "javascript",
+    "demanded_count": "20",
+    "avg_salary": "97587.00"
+  }
+]
 */
